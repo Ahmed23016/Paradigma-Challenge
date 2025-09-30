2f115ef254ee62947b9d98b03cfc67bbe83e0b62
Ahmed <amkistan@gmail.com>
2025-09-30 12:21:23 +0200

    Create printen.clj

---
+++ code/tekstanalyse/src/seradrev/printen.clj
@@ -0,0 +1,11 @@
+(ns seradrev.printen
+  (:gen-class))
+
+;; Heel simpel voorbeeld: iets printen naar de console
+(defn -main [& _]
+  (println "Hallo Hoi. !??@?#$^^@# ")
+  (println "Water wateR WateR WaTeR ")
+  (println "h"))
+
+;; runnen door clj -M -m seradrev.printen
+
