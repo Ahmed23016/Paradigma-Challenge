e00b7794a92958f8d1817b269f9205c5bcd8c57a
Ahmed <amkistan@gmail.com>
2025-09-30 11:58:37 +0200

    start

--- code/tekstanalyse/src/seradrev/tekstanalyse.clj
+++ code/tekstanalyse/src/seradrev/tekstanalyse.clj
@@ -1,13 +1,22 @@
 (ns seradrev.tekstanalyse
   (:gen-class))
 
-(defn greet
-  "Callable entry point to the application."
-  [data]
-  (println (str "Hello, " (or (:name data) "World") "!")))
+(defn countdown
+  [n]
+  (when (>= n 0)
+    (println n)
+    (when (> n 0)
+      (countdown (dec n)))))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (greet {:name (first args)}))
-
+  [& _]
+  ;; bestand lezen
+  (let [content (slurp "resources/lol.txt")
+        words   (clojure.string/split content #"\s+")
+        count   (count words)]
+    (println "Bestandsinhoud:")
+    (println content)
+    (println words)
+    (println "\nAantal woorden:" count)
+    (println "\nCountdown vanaf aantal woorden:")
+    (countdown count)))
