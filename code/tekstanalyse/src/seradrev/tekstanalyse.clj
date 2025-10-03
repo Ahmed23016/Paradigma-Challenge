b77838f81ce1b169330b7d264d65abc2fd0e94ea
Ahmed <amkistan@gmail.com>
2025-10-03 12:20:39 +0200

    sorteer array van hoog naar laag (insertion sort)

--- code/tekstanalyse/src/seradrev/tekstanalyse.clj
+++ code/tekstanalyse/src/seradrev/tekstanalyse.clj
@@ -2,47 +2,62 @@
   (:gen-class)
   (:require [clojure.string :as str]))
 
-;; maakt de tekst schoon en splitst het in losse woorden
+;; maakt tekst schoon en splitst in losse woorden
 (defn woorden [text]
   (-> text
       str/lower-case                     ;; alles naar kleine letters
       (str/replace #"[^\p{L}\s]" "")     ;; alle leestekens eruithale
       (str/split #"\s+")))               ;; tekst opsplitsen op spaties
 
-;; zoekt handmatig een waarde op in een lijst van paren
-(defn mijn-get [freq-lijst gezocht-woord]
-  (if (empty? freq-lijst)                ;; als de lijst leeg is stoppe we hier
-    nil                                  ;; dan is het woord nie gevonden
-    (let [[huidig-woord huidig-aantal] (first freq-lijst)]  ;; pak het eerste woord en aantal uit de lijst
-      (if (= huidig-woord gezocht-woord)  ;; check of dit het woord is dat we zoeken
-        huidig-aantal                     ;; als dat zo is geef het aantal terug
-        (mijn-get (rest freq-lijst) gezocht-woord))))) ;; anders zoeken we verder in de rest van de lijst
+;; zoekt of een woord al in de lijst zit en geeft het aantal terug
+(defn zoek-woord [woorden-en-aantallen gezocht]
+  (if (empty? woorden-en-aantallen)          ;; lijst leeg? dan niks gevonden
+    nil
+    (let [[woord-nu aantal-nu] (first woorden-en-aantallen)] ;; pak eerste paar
+      (if (= woord-nu gezocht)               ;; is dit het woord?
+        aantal-nu                           ;; ja → geef aantal terug
+        (zoek-woord (rest woorden-en-aantallen) gezocht))))) ;; anders verder zoeken
 
-;; voegt handmatig een (woord aantal)-paar toe of vervangt het als het er al is
-(defn mijn-assoc [freq-lijst nieuw-woord nieuw-aantal]
-  (if (empty? freq-lijst)                ;; als de lijst leeg is doen we gewoon het eerste element erin
-    (cons [nieuw-woord nieuw-aantal] '()) ;; voeg dit woord en aantal als eerst toe
-    (let [[huidig-woord huidig-aantal] (first freq-lijst)] ;; pak het eerste woord en aantal uit de lijstt
-      (if (= huidig-woord nieuw-woord)   ;; check of dit het woord is dat we wille update
-        (cons [nieuw-woord nieuw-aantal] (rest freq-lijst)) ;; vervang het met het nieuwe aantal dat we nu hebben
-        (cons [huidig-woord huidig-aantal] (mijn-assoc (rest freq-lijst) nieuw-woord nieuw-aantal)))))) ;; anders laten we dit woord staan en gaan verder zo door
+;; voegt een woord toe of update het aantal als het al bestaat
+(defn voeg-toe-of-update [woorden-en-aantallen nieuw-woord nieuw-aantal]
+  (if (empty? woorden-en-aantallen)                     ;; leeg? voeg gewoon toe
+    (cons [nieuw-woord nieuw-aantal] '())
+    (let [[woord-nu aantal-nu] (first woorden-en-aantallen)] ;; pak eerste paar
+      (if (= woord-nu nieuw-woord)                     ;; zelfde woord?
+        (cons [nieuw-woord nieuw-aantal] (rest woorden-en-aantallen)) ;; vervang aantal
+        (cons [woord-nu aantal-nu] (voeg-toe-of-update (rest woorden-en-aantallen) nieuw-woord nieuw-aantal)))))) ;; anders verder
 
-;; telt hoe vaak elk woord voorkomt in de tekst
-(defn frequentie
-  ([woorden-lijst] (frequentie woorden-lijst '())) ;; start met lege frequentielijst want we hebben nog niks
-  ([woorden-lijst freq-lijst]
-   (if (empty? woorden-lijst)            ;; als er geen woorden meer zijn dan zijn we klaar
-     freq-lijst                          ;; dan geven we de lijst terug
-     (let [huidig-woord (first woorden-lijst) ;; pak het eerste woord uit de lijst zodat we ermee kunnen werken
-           bestaand-aantal (mijn-get freq-lijst huidig-woord) ;; check of dit woord al eerder is getelt
-           nieuw-aantal (if bestaand-aantal (inc bestaand-aantal) 1) ;; verhoog het aantal of zet het op 1 als het nieuw woord is
-           bijgewerkte-freq (mijn-assoc freq-lijst huidig-woord nieuw-aantal)] ; voeg dit nieuwe aantal toe aan de lijst zodat het klopt
-       (frequentie (rest woorden-lijst) bijgewerkte-freq))))) ;; doe hetzelfde voor de rest van de woorden. recursief en herhaal tot klaar :)
+;; telt hoe vaak elk woord voorkomt
+(defn tel-woorden
+  ([lijst] (tel-woorden lijst '())) ;; start met lege lijst
+  ([lijst woorden-en-aantallen]
+   (if (empty? lijst)               ;; klaar? geef lijst terug
+     woorden-en-aantallen
+     (let [woord-nu (first lijst)                                 ;; pak eerste woord
+           aantal-nu (zoek-woord woorden-en-aantallen woord-nu)   ;; zoek huidig aantal
+           nieuw-aantal (if aantal-nu (inc aantal-nu) 1)          ;; verhoog of zet op 1
+           nieuwe-lijst (voeg-toe-of-update woorden-en-aantallen woord-nu nieuw-aantal)] ;; update lijst
+       (tel-woorden (rest lijst) nieuwe-lijst)))))                ;; verder met de rest
 
-;; leest het bestand, maakt array(vector) van woorden en telt frequentie
+;; voegt een woord op de juiste plek toe in een gesorteerde lijst
+(defn voeg-op-plek [element gesorteerd]
+  (if (empty? gesorteerd)                    ;; leeg? maak nieuwe lijst
+    (list element)
+    (let [[[woord aantal] & rest] gesorteerd] ;; pak eerste paar
+      (if (>= (second element) aantal)       ;; groter of gelijk? hier invoegen
+        (cons element gesorteerd)
+        (cons [woord aantal] (voeg-op-plek element rest)))))) ;; anders verder
+
+;; sorteert de lijst van hoog naar laag
+(defn sorteer-lijst [woorden-en-aantallen]
+  (if (empty? woorden-en-aantallen) '()
+      (voeg-op-plek (first woorden-en-aantallen) (sorteer-lijst (rest woorden-en-aantallen)))))
+
+;; hoofdprogramma
 (defn -main [& _]
-  (let [inhoud (slurp "resources/lol.txt")         ;; lees de tekst uit het bestand (zorg dat het bestaat)
-        woorden-lijst (woorden inhoud)            ;; maak een lijst van alle woorden die we hebben
-        frequentie-lijst (frequentie woorden-lijst)] ;; tel hoe vaak elk woord voorkommt in de tekst
-    (println "woordfrequentie in de tekst")      
-    (println frequentie-lijst)))                 
+  (let [inhoud (slurp "resources/lol.txt")         ;; lees tekstbestand
+        woorden-lijst (woorden inhoud)            ;; maak woordenlijst
+        geteld (tel-woorden woorden-lijst)        ;; tel alle woorden
+        gesorteerd (sorteer-lijst geteld)]        ;; sorteer van hoog naar laag
+    (println "woordfrequentie gesorteerd van hoog naar laag")
+    (println gesorteerd)))
